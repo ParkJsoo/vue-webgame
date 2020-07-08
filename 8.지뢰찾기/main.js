464b2aea42b66516795825f2f36925c5f966b305
ParkJsoo <misty0314@naver.com>
2020-07-08 16:11:26 +0900

    8. 지뢰찾기
    8.2 지뢰찾기 코드 부여하기
    8.3 데이터 가공해 화면 그리기
    8.4 타이머 켜고 끄기

--- 8.지뢰찾기/main.js
+++ 8.지뢰찾기/main.js
@@ -1,3 +1,4 @@
-import ...
+import Vue from 'vue';
+import TicTacToe from './MineSweeper';
 
-new Vue(TicTacToe).$mount("#root");
+new Vue(TicTacToe).$mount('#root');
--- 8.지뢰찾기/store.js
+++ 8.지뢰찾기/store.js
@@ -11,6 +11,46 @@ export const QUESTION_CELL = 'QUESTION_CELL';
 export const NORMALIZE_CELL = 'NORMALIZE_CELL';
 export const INCREMENT_TIMER = 'INCREMENT_TIMER';
 
+export const CODE = {
+  MINE: -7,
+  NORMAL: -1,
+  QUESTION: -2,
+  FLAG: -3,
+  QUESTION_MINE: -4,
+  FLAG_MINE: -5,
+  CLICKED_MINE: -6,
+  OPENED: 0, // 0 이상이면 다 opened
+};
+
+const plantMine = (row, cell, mine) => {
+  console.log(row, cell, mine);
+  const candidate = Array(row * cell).fill().map((arr, i) => {
+    return i;
+  });
+  const shuffle = [];
+  while (candidate.length > row * cell - mine) {
+    const chosen = candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0];
+    shuffle.push(chosen);
+  }
+  const data = [];
+  for (let i = 0; i < row; i++) {
+    const rowData = [];
+    data.push(rowData);
+    for (let j = 0; j < cell; j++) {
+      rowData.push(CODE.NORMAL);
+    }
+  }
+
+  for (let k = 0; k < shuffle.length; k++) {
+    const ver = Math.floor(shuffle[k] / cell);
+    const hor = shuffle[k] % cell;
+    data[ver][hor] = CODE.MINE;
+  }
+
+  console.log(data);
+  return data;
+};
+
 export default new Vuex.Store({
   state: {
     tableData: [],
@@ -20,6 +60,7 @@ export default new Vuex.Store({
       mine: 0,
     },
     timer: 0,
+    halted: true, // 중단된
     result: '',
   }, // vue의 data와 비슷
   getters: {
@@ -27,13 +68,22 @@ export default new Vuex.Store({
   }, // vue의 computed와 비슷
   mutations: {
     [START_GAME](state, {row, cell, mine }) {
-      
+      state.data = {
+        row,
+        cell,
+        mine,
+      };
+      state.tableData = plantMine(row, cell, mine);
+      state.timer = 0;
+      state.halted = false;
     },
     [OPEN_CELL](state) {},
     [CLICK_MINE](state) {},
     [FLAG_CELL](state) {},
     [QUESTION_CELL](state) {},
     [NORMALIZE_CELL](state) {},
-    [INCREMENT_TIMER](state) {},
+    [INCREMENT_TIMER](state) {
+      state.timer += 1;
+    },
   }, // state를 수정할 때 사용. 동기적으로
 });
