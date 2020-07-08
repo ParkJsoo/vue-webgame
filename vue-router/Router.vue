3d12a8f9d1ad97f4c9be437615aba608ac1c540e
ParkJsoo <misty0314@naver.com>
2020-07-08 22:56:11 +0900

    9. Vue Router
    9.1 Vue Router 설정하기

---
+++ vue-router/Router.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    import router from './routes';
+
+    export default {
+       router,
+    };
+</script>
+
+<style>
+    table {
+        border-collapse: collapse;
+    }
+    td {
+        border: 1px solid black;
+        width: 40px;
+        height: 40px;
+        text-align: center;
+    }
+</style>
